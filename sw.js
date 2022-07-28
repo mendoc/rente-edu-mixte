64a8b7bcb5ef7db0d22c60cea073a6aaf360c930
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-07-28 16:06:39 +0000

    Implémente le mode déconnecté

--- sw.js
+++ sw.js
@@ -1,33 +1,49 @@
-const staticCacheName = 'rente-education-1-00-00';
+const cacheName = 'rente-education-1-00-04';
 const assets = [
     '/',
     '/index.html',
 ];
+
+var mortalite = [];
+for (var i = 0; i <= 106; i++) {
+    assets.push('/mortalite/' + i + '.json');
+}
+
+
 // install event
 self.addEventListener('install', evt => {
     evt.waitUntil(
-        caches.open(staticCacheName).then((cache) => {
-            console.log('caching shell assets');
+        caches.open(cacheName).then((cache) => {
+            console.log('Enregistrement des assets dans le cache');
             cache.addAll(assets);
         })
     );
 });
+
 // activate event
 self.addEventListener('activate', evt => {
     evt.waitUntil(
         caches.keys().then(keys => {
             return Promise.all(keys
-                .filter(key => key !== staticCacheName)
+                .filter(key => key !== cacheName)
                 .map(key => caches.delete(key))
             );
         })
     );
 });
-// fetch event
-self.addEventListener('fetch', evt => {
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Récupération de la ressource: ' + e.request.url);
+            return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+                    console.log('[Service Worker] Mise en cache de la nouvelle ressource: ' + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
+
